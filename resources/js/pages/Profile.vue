315f6813582ed640b2ce7608dcc30960fc74ebb3
scottishsummer98 <samiremail98@gmail.com>
2022-11-27 23:32:14 +0600

    'imagegridphotoiniti'

--- resources/js/pages/Profile.vue
+++ resources/js/pages/Profile.vue
@@ -225,7 +225,9 @@
   </div>
   <div class="friends-profile-block" v-if="component == 'friends'"></div>
   <div class="pages-profile-block" v-if="component == 'pages'"></div>
-  <div class="photos-profile-block" v-if="component == 'photos'"></div>
+  <div class="photos-profile-block" v-if="component == 'photos'">
+    <lightbox cells="4" :items="images"></lightbox>
+  </div>
   <div class="edit-profile-block" v-if="component == 'edit'">
     <form
       @submit.prevent
@@ -683,6 +685,8 @@
 </template>
 
 <script>
+import '@morioh/v-lightbox/dist/lightbox.css'
+import Lightbox from '@morioh/v-lightbox'
 import moment from 'moment'
 import { showSuccess, showError } from '../helper'
 export default {
@@ -716,6 +720,28 @@ export default {
       editingItem: {
         desc: '',
       },
+      images: [
+        'https://i.wifegeek.com/200426/f9459c52.jpg',
+        'https://i.wifegeek.com/200426/5ce1e1c7.jpg',
+        'https://i.wifegeek.com/200426/5fa51df3.jpg',
+        'https://i.wifegeek.com/200426/663181fe.jpg',
+        'https://i.wifegeek.com/200426/2d110780.jpg',
+        'https://i.wifegeek.com/200426/e73cd3fa.jpg',
+        'https://i.wifegeek.com/200426/15160d6e.jpg',
+        'https://i.wifegeek.com/200426/d0c881ae.jpg',
+        'https://i.wifegeek.com/200426/a154fc3d.jpg',
+        'https://i.wifegeek.com/200426/71d3aa60.jpg',
+        'https://i.wifegeek.com/200426/d17ce9a0.jpg',
+        'https://i.wifegeek.com/200426/7c4deca9.jpg',
+        'https://i.wifegeek.com/200426/64672676.jpg',
+        'https://i.wifegeek.com/200426/de6ab9c6.jpg',
+        'https://i.wifegeek.com/200426/d8bcb6a7.jpg',
+        'https://i.wifegeek.com/200426/4085d03b.jpg',
+        'https://i.wifegeek.com/200426/177ef44c.jpg',
+        'https://i.wifegeek.com/200426/d74d9040.jpg',
+        'https://i.wifegeek.com/200426/81e24a47.jpg',
+        'https://i.wifegeek.com/200426/43e2e8bb.jpg',
+      ],
     }
   },
   methods: {
