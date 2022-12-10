f87a4d7c0c68d1a8e8cff0742707011e46578dfa
scottishsummer98 <samiremail98@gmail.com>
2022-12-10 18:40:57 +0600

    'Pagesinit'

--- app/Models/Pages.php
+++ app/Models/Pages.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Pages extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'owner'];
 }
--- database/migrations/2022_12_10_123825_create_pages_table.php
+++ database/migrations/2022_12_10_123825_create_pages_table.php
@@ -15,6 +15,8 @@ class CreatePagesTable extends Migration
     {
         Schema::create('pages', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('owner');
             $table->timestamps();
         });
     }
