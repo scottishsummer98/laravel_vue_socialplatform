0275c2079cd0ef2187fcd356dfc2483880732a92
scottishsummer98 <samiremail98@gmail.com>
2022-12-11 10:14:50 +0600

    'followersmodelmigrations'

---
+++ database/migrations/2022_12_11_035706_create_followers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFollowersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('followers', function (Blueprint $table) {
+            $table->id();
+            $table->string('userid');
+            $table->string('addedfriends', 5000);
+            $table->string('pendingfriends', 5000);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('followers');
+    }
+}
