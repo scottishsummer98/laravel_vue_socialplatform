747d8c1ba07ede2abfc9489a96f258551719d222
scottishsummer98 <samiremail98@gmail.com>
2023-01-06 23:48:27 +0600

    'FriendsPageSuggestionsPart'

--- resources/js/pages/Friends.vue
+++ resources/js/pages/Friends.vue
@@ -40,6 +40,39 @@
       </div>
     </div>
     <br />
+    <div>
+      <h3 class="text-white">Friends</h3>
+      <div>
+        <swiper :slides-per-view="4" :space-between="2">
+          <swiper-slide v-for="(item, index) in friendlist" :key="index">
+            <div>
+              <img
+                :src="`../storage/${item.dp}`"
+                alt=""
+                style="width: 12rem; height: 12rem; padding-bottom: 1rem;"
+              />
+              <h6 class="text-white">
+                <router-link
+                  :to="`/profile/${item.id}`"
+                  style="text-decoration: none; color: white;"
+                >
+                  {{ item.fname }} {{ item.lname }}
+                </router-link>
+              </h6>
+              <br />
+              <button
+                class="btn btn-block btn-spooky"
+                style="border: 1px solid black;"
+                @click="unfriend(item.id)"
+              >
+                Unfriend
+              </button>
+            </div>
+          </swiper-slide>
+        </swiper>
+      </div>
+    </div>
+    <br />
   </div>
 </template>
 
