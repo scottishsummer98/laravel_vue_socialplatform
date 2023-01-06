d7f9a80e8515d70827ea2e5ef80285da6426e78a
scottishsummer98 <samiremail98@gmail.com>
2023-01-06 23:49:07 +0600

    'FriendslISTPART'

--- resources/js/pages/Friends.vue
+++ resources/js/pages/Friends.vue
@@ -73,6 +73,58 @@
       </div>
     </div>
     <br />
+    <div>
+      <h3 class="text-white">Friend Suggestions</h3>
+      <div>
+        <swiper :slides-per-view="4" :space-between="2">
+          <swiper-slide v-for="(item, index) in friendsugglist" :key="index">
+            <div>
+              <img
+                :src="`../storage/${item.dp}`"
+                alt=""
+                style="width: 12rem; height: 12rem; padding-bottom: 1rem;"
+              />
+              <h6 class="text-white">
+                <router-link
+                  :to="`/profile/${item.id}`"
+                  style="text-decoration: none; color: white;"
+                >
+                  {{ item.fname }} {{ item.lname }}
+                </router-link>
+              </h6>
+              <br />
+              <div v-if="item.pendingfriends != null">
+                <button
+                  v-if="item.pendingfriends.includes(user.id)"
+                  class="btn btn-block btn-spooky"
+                  style="border: 1px solid black;"
+                  @click="cancelRequest(item.id)"
+                >
+                  Friend Request Sent
+                </button>
+                <button
+                  v-else
+                  class="btn btn-block btn-spooky"
+                  style="border: 1px solid black;"
+                  @click="addFriend(item.id)"
+                >
+                  Add Friend
+                </button>
+              </div>
+              <div v-else>
+                <button
+                  class="btn btn-block btn-spooky"
+                  style="border: 1px solid black;"
+                  @click="addFriend(item.id)"
+                >
+                  Add Friend
+                </button>
+              </div>
+            </div>
+          </swiper-slide>
+        </swiper>
+      </div>
+    </div>
   </div>
 </template>
 
