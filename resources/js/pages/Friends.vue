f3092cfdf107029e45fc27a09788235b54d53b9f
scottishsummer98 <samiremail98@gmail.com>
2023-01-06 23:47:29 +0600

    'FriendsPage'

--- resources/js/pages/Friends.vue
+++ resources/js/pages/Friends.vue
@@ -1,11 +1,46 @@
 <template>
-  <div class="row">
-    Friends
+  <div class="friends-profile-block">
+    <div>
+      <h3 class="text-white">Friend Requests</h3>
+      <div>
+        <swiper :slides-per-view="4" :space-between="2">
+          <swiper-slide v-for="(item, index) in friendreqlist" :key="index">
+            <div>
+              <img
+                :src="`../storage/${item.dp}`"
+                alt=""
+                style="width: 12rem; height: 12rem; padding-bottom: 1rem;"
+              />
+              <h6 class="text-white">
+                <router-link
+                  :to="`/profile/${item.id}`"
+                  style="text-decoration: none; color: white;"
+                >
+                  {{ item.fname }} {{ item.lname }}
+                </router-link>
+              </h6>
+              <br />
+              <button
+                class="btn btn-block btn-spooky"
+                style="border: 1px solid black;"
+                @click="aceeptFriend(item.id)"
+              >
+                Accept
+              </button>
+              <button
+                class="btn btn-block btn-spooky"
+                style="border: 1px solid black;"
+                @click="removeRequest(item.id)"
+              >
+                Remove
+              </button>
+            </div>
+          </swiper-slide>
+        </swiper>
+      </div>
+    </div>
+    <br />
   </div>
 </template>
-
-<script>
-export default {}
-</script>
 
 <style></style>
