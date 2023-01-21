efd9f2b8eba5fd594102fe248d3f77c904278d72
scottishsummer98 <samiremail98@gmail.com>
2023-01-21 21:15:11 +0600

    'dbupdate'

--- socialplatform.sql
+++ socialplatform.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 12, 2023 at 02:57 PM
+-- Generation Time: Jan 21, 2023 at 03:13 PM
 -- Server version: 5.7.33
 -- PHP Version: 7.4.27
 
