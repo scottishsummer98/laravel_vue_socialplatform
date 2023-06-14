f16c394549e39cf1a823809ae5c5dd72f4f5f707
scottishsummer98 <samiremail98@gmail.com>
2023-06-14 08:47:55 +0600

    'sanctumfunctionsforauth'

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -53,4 +53,72 @@ class AuthController extends Controller
     {
         Auth::logout();
     }
+
+    //Sanctum Functions
+    // public  function register(Request $request)
+    // {
+    //     $request->validate([
+    //         'fname' => ['required'],
+    //         'lname' => ['required'],
+    //         'dob' => ['required'],
+    //         'gender' => ['required'],
+    //         'mobile' => ['required'],
+    //         'email' => ['required', 'email', 'unique:users'],
+    //         'password' => ['required', 'min:6', 'confirmed'],
+    //     ]);
+
+    //     $user = User::create([
+    //         'fname' => $request->fname,
+    //         'lname' => $request->lname,
+    //         'dob' => $request->dob,
+    //         'gender' => $request->gender,
+    //         'mobile' => $request->mobile,
+    //         'email' => $request->email,
+    //         'password' => Hash::make($request->password),
+    //     ]);
+
+    //     $token = $user->createToken('myapptoken')->plainTextToken;
+
+    //     $response = [
+    //         'user' => $user,
+    //         'token' => $token,
+    //     ];
+
+    //     return response($response, 201);
+    // }
+
+    // public  function login(Request $request)
+    // {
+    //     $request->validate([
+    //         'email' => ['required', 'email'],
+    //         'password' => ['required'],
+    //     ]);
+
+    //     //Check Email
+    //     $user = User::where('email', $request->email)->first();
+
+    //     //Check Password
+    //     if (!$user || !Hash::check($request->password, $user->password)) {
+    //         return response([
+    //             'message' => 'Credentials Didn\'t match!'
+    //         ], 401);
+    //     }
+
+    //     $token = $user->createToken('myapptoken')->plainTextToken;
+
+    //     $response = [
+    //         'user' => $user,
+    //         'token' => $token,
+    //     ];
+
+    //     return response($response, 201);
+    // }
+
+    // public function logout()
+    // {
+    //     auth('sanctum')->user()->tokens()->delete();
+    //     return [
+    //         'message' => 'Logged Out'
+    //     ];
+    // }
 }
